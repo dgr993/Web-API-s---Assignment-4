6a1a25fb6b1ac0b1b4b73821f2b6f1fa54d171b0
Wuzup <dgr993@gmail.com>
2020-06-13 20:09:51 -0600

    added the html of the timer and the button to start the timer

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ var startBtn= document.querySelector(".startBtn")
 var questionsH1 = document.querySelector(".questions")
 var appendHere = document.querySelector(".appendHere")
 var index = 0
+var answerBtn = document.querySelector(".answerBtn")
 
 var questionsArray=[
     {
@@ -16,7 +17,36 @@ var questionsArray=[
         options:["big size for header","like field", "return field"],
         answer:"big size for header"
     },
-
+    {
+        question:"What is an h3 tag?",
+        options:["or not","please erase", "thisis the answer"],
+        answer:"big size for header"
+    },
+    {
+        question:"What is an h4 tag?",
+        options:["Okay","yep", "weird"],
+        answer:"big size for header"
+    },
+    {
+        question:"What is an h5 tag?",
+        options:["oh my","test", "this"],
+        answer:"big size for header"
+    },
+    {
+        question:"What is an h1 tag?",
+        options:["big size for header","like field", "return field"],
+        answer:"big size for header"
+    },
+    {
+        question:"What is an h1 tag?",
+        options:["big size for header","like field", "return field"],
+        answer:"big size for header"
+    },
+    {
+        question:"What is an h1 tag?",
+        options:["big size for header","like field", "return field"],
+        answer:"big size for header"
+    }
 ]
 
 questionsDiv.style.display= "none"
@@ -44,16 +74,12 @@ function showQuestions(){
         
     }
     appendHere.appendChild(myDiv)
-
-    answerBtn.addEventListener("click", function(){
-    
-        alert("this is the answer")
-        index++;
-        showQuestions();
-    });
-
-    
 }
-
+answerBtn.addEventListener("click", function(){
+    
+    alert("this is the answer")
+    index++;
+    showQuestions();
+});
 
 // event listern to answerBtn , and when clicked you want to add to index , and run show questions function again
